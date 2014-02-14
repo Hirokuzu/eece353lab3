f1f841602f5f10a946967ecbb37bef0ebaa9af42
Hirokuzu <arnold.hn.ip@gmail.com>
2014-02-14 08:16:16 -0800

    Updated state machine
    Yet to be complete, but state types were defined, and states were
    defined up to deal_player_3.

--- statemachine.vhd
+++ statemachine.vhd
@@ -36,8 +36,62 @@ ARCHITECTURE behavioural OF statemachine IS
             current_game_state := reset;
         else if(rising_edge(slow_clock)) then
             case(current_game_state) is
-                when 
-        end if
+                when reset => -- the loads are done this way to make sense of the order
+                    load_pcard1 <= '0';
+                    load_dcard1 <= '0';
+                    load_pcard2 <= '0';
+                    load_dcard2 <= '0';
+                    load_pcard3 <= '0';
+                    load_dcard3 <= '0';
+                    LEDG <= "00";
+                    current_game_state := deal_player_1;
+                when deal_player_1 =>
+                    load_pcard1 <= '1';
+                    current_game_state := deal_dealer_1;
+                when deal_dealer_1 =>
+                    load_pcard1 <= '0';
+                    load_dcard1 <= '1';
+                    current_game_state := deal_player_2;
+                when deal_player_2 =>
+                    load_dcard1 <= '0';
+                    load_pcard2 <= '1';
+                    current_game_state := deal_dealer_2;
+                when deal_dealer_2 =>
+                    load_pcard2 <= '0';
+                    load_dcard2 <= '1';
+                    if((dscore > "1000") or (pscore > "1000")) then
+                        current_game_state := determine_victor;
+                    else if((pscore >= "0000") and (pscore <= "0101")) then
+                        current_game_state := deal_player_3;
+                    else if((pscore >= "0110") and (pscore <= "0111")) then
+                        if((dscore >= "0000") and (dscore <= "0101")) then
+                            current_game_state := deal_dealer_3;
+                        end if;
+                    end if;
+                when deal_player_3 =>
+                    load_dcard_2 <= '0';
+                    load_pcard_3 <= '1';
+                    -- tons of crap here...
+                    if((dscore >= "0000") and (dscore <= "0010") then
+                        current_game_state := deal_dealer_3;
+                    else if(dscore = "0011") then
+                        if(pcard3 = 8) then
+                            current_game_state := determine_winner;
+                        else
+                            current_game_state := deal_dealer_3;
+                        end if;
+                    else if(dscore = "") then
+                    else if(dscore = "") then
+                    else if(dscore = "") then
+                    else if(dscore = "") then
+                    end if;
+                when deal_dealer_3 =>
+                    load_pcard_2 <= '0'; -- dunno which state it came from, so gotta make sure its zero
+                    load_pcard_3 <= '0';
+                    load_dcard_3 <= '1';
+                when determine_victor =>
+            end case;
+        end if;
     end process;
 
 END;
